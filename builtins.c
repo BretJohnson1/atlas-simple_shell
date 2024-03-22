cec9ea2ffaca090a459fca45582f8de3a64da88e
BretJohnson1 <155486952+BretJohnson1@users.noreply.github.com>
2024-03-22 13:40:24 -0500

    Creating builtins.c file

---
+++ builtins.c
@@ -0,0 +1,24 @@
+#include "simple_shell.h"
+/**
+*quit_repl - quit interactive mode
+*@shell: double pointer back to shell interpreter
+*Description: placeholder
+*/
+void quit_repl(SimpleShell_t *shell)
+{
+    (shell->is_active) = FALSE;
+}
+
+/**
+*print_env_variables - print list of environmental vaiables
+*@shell: double pointer back to shell interpreter
+*Description: placeholder
+*/
+void print_env_variables(SimpleShell_t *shell)
+{
+    int i = 0;
+
+    do {
+        printf("%s\n", shell->environment[i++]);
+    } while (shell->environemt[i]);
+}
