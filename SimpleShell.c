0b0a628b91156746c93f1ff8161b3ff16e4eb7fa
Dtbissy <isaactheedwards@gmail.com>
2024-03-27 19:12:59 +0000

    Simple Shell betty finally

--- SimpleShell.c
+++ SimpleShell.c
@@ -1,33 +1,33 @@
 #include "simple_shell.h"
+
 /**
 *create_shell - allocate memory for the simple shell and set values
 *@shell: double pointer back to the simple shell
 *@envp: environmental variables passed directly from main
 *
-*Return: whether or not the shell was successfully created
+*Return: whether or not the shell was successfully creatd
 */
 void create_shell(SimpleShell_t **shell, char **envp)
 {
-/**allocates memory for the shell structure and assign it to the pointer*/
-	*shell = malloc(sizeof(SimpleShell_t));
+*shell = malloc(sizeof(SimpleShell_t));
 /**check if allocation was successful*/
-	if (shell == NULL)
-		return;
+if (shell == NULL)
+	return;
 /**initializing various members of the structure*/
 /**set line number to 0*/
-	((*shell)->line_num) = 0;
+((*shell)->line_num) = 0;
 /**set exit status to 0*/
-	((*shell)->exit_status) = 0;
+((*shell)->exit_status) = 0;
 /**set the shell as active*/
-	((*shell)->is_active) = TRUE;
+((*shell)->is_active) = TRUE;
 /**copy envrionment variables into the shell*/
-	(*shell)->environment = envp;
+(*shell)->environment = envp;
 /**split path variables into an array of directories and assign it to path_variable*/
-	(*shell)->path_variable = split_string(getenv("PATH"), ":");
+(*shell)->path_variable = split_string(getenv("PATH"), ":");
 /**set the os_command_path to NULL initially*/
-	(*shell)->os_command_path = NULL;
+(*shell)->os_command_path = NULL;
 /**set the builtin command to NULL initially*/
-	(*shell)->builtin = NULL;
+(*shell)->builtin = NULL;
 }
 
 /**
@@ -40,24 +40,24 @@ void create_shell(SimpleShell_t **shell, char **envp)
 void parse_line(SimpleShell_t *shell, char *new_line)
 {
 /**split the input line into individual command arguments*/
-	shell->command_args = split_string(new_line, " ");
+shell->command_args = split_string(new_line, " ");
 /**determine if the command is a builtin command and assign the correspeonding function pointer*/
-	shell->builtin = get_builtin(shell->command_args[0]);
+shell->builtin = get_builtin(shell->command_args[0]);
 /**find the path of the os command executable*/
-	shell->os_command_path = find_command_path(shell);
+shell->os_command_path = find_command_path(shell);
 /**check if the command is a built in command*/
-	if (shell->builtin != NULL)
-/**if it is a built in command, execute it*/
-		shell->builtin(shell);
+if (shell->builtin != NULL)
+	/**if it is a built in command, execute it*/
+	shell->builtin(shell);
 /**if the command is not a built in but the os command path is found, create a new process to execute it*/
-	else if (shell->os_command_path != NULL)
-		create_new_process(shell);
+else if (shell->os_command_path != NULL)
+	create_new_process(shell);
 /**if it is niether a built in or os, then throw error "not found"*/
-	else
-		throw_error(shell, 2);
+else
+	throw_error(shell, 2);
 
-	free_array(shell->command_args);
-	free(shell->os_command_path);
+free_array(shell->command_args);
+free(shell->os_command_path);
 }
 /**
 *free_shell - deallocates memory for the interpreter and its priorities
@@ -65,9 +65,9 @@ void parse_line(SimpleShell_t *shell, char *new_line)
 */
 void free_shell(SimpleShell_t *shell)
 {
-	free_array(shell->path_variable);
+free_array(shell->path_variable);
 
-	free(shell);
+free(shell);
 }
 
 /**
@@ -76,12 +76,12 @@ void free_shell(SimpleShell_t *shell)
 */
 void free_array(char **an_array)
 {
-	int i;
+int i;
 
-	for (i = 0; an_array[i]; i++)
-		free(an_array[i]);
+for (i = 0; an_array[i]; i++)
+	free(an_array[i]);
 
-	free(an_array);
+free(an_array);
 }
 
 /**
@@ -92,22 +92,22 @@ void free_array(char **an_array)
 */
 void (*get_builtin(char *command))(SimpleShell_t *)
 {
-	int i;
+int i;
+
 /**defining an array of structures representing builtin commands*/
-	BuiltInCommand_t builtins[] = {
-		{"exit", quit_repl},
-		{"quit", quit_repl},
-		{"env", print_env_variables},
-		{NULL, NULL}
-	};
+BuiltInCommand_t builtins[] = {
+	{"exit", quit_repl},
+	{"quit", quit_repl},
+	{"env", print_env_variables},
+	{NULL, NULL}};
 /**loop through the array of builtin commands*/
-	for (i = 0; builtins[i].name; i++)
-	{
-/**check if the current command matches any builtin command*/
-		if (strcmp(command, builtins[i].name) == 0)
-/**if a match is found, return the corresponding function pointer*/
-			return (builtins[i].exec);
-	}
+for (i = 0; builtins[i].name; i++)
+{
+	/**check if the current command matches any builtin command*/
+	if (strcmp(command, builtins[i].name) == 0)
+	/**if a match is found, return the corresponding function pointer*/
+	return (builtins[i].exec);
+}
 /**if no match is found, return NULL*/
-	return (NULL);
+return (NULL);
 }
