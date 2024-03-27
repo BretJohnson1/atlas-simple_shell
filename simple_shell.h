4690c67d7261b25f9cc3cd42c327a8012513e078
Dtbissy <isaactheedwards@gmail.com>
2024-03-27 20:29:02 +0000

    The simple_shell.h file is compliant

--- simple_shell.h
+++ simple_shell.h
@@ -27,15 +27,22 @@
 */
 typedef struct _SimpleShell
 {
-    int line_num;
-    int exit_status;
-    int is_active;
-    char **path_variable;
-    char **environment;
-    void (*builtin)(struct _SimpleShell *);
-    char *os_command_path;
-    char **command_args;
-}SimpleShell_t;
+int line_num;
+
+int exit_status;
+
+int is_active;
+
+char **path_variable;
+
+char **environment;
+
+void (*builtin)(struct _SimpleShell *);
+char *os_command_path;
+
+char **command_args;
+
+} SimpleShell_t;
 
 /**
 *struct BuiltInCommand_s - information to build table of built in commands
@@ -44,24 +51,30 @@ typedef struct _SimpleShell
 */
 typedef struct BuiltInCommand_s
 {
-    char *name;
-    void (*exec)(SimpleShell_t *self);
-}BuiltInCommand_t;
+char *name;
+
+void (*exec)(SimpleShell_t *self);
+} BuiltInCommand_t;
 
 void launch_repl(SimpleShell_t *shell);
 void read_script(SimpleShell_t *shell);
 
 void create_shell(SimpleShell_t **shell, char **envp);
+
 void parse_line(SimpleShell_t *shell, char *new_line);
+
 void free_shell(SimpleShell_t *shell);
 void free_array(char **an_array);
 
 char *take_input();
 int is_only_spaces(char *string);
+
 int num_of_spaces(char *string, char *delimiter);
+
 char **split_string(char *string_to_split, char *delimiter);
 
 void (*get_builtin(char *command))(SimpleShell_t *);
+
 void clear_screen(SimpleShell_t *self);
 void quit_repl(SimpleShell_t *self);
 void print_env_variables(SimpleShell_t *self);
@@ -69,6 +82,7 @@ char *_getenv(char **envp, char *key);
 
 char *find_command_path(SimpleShell_t *self);
 char *create_test_path(char *dir_path, char *command);
+
 void create_new_process(SimpleShell_t *shell);
 
 void throw_error(SimpleShell_t *shell, int error_num);
